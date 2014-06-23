3b513d7208ac21621298b32be2b000cffb2e0023
cevalloa <cevalloa@gmail.com>
2014-06-23 16:53:18 -0400

    Initial Commit

---
+++ Macys_iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Macys_iOS
+//
+//  Created by Alex Cevallos on 6/23/14.
+//  Copyright (c) 2014 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Macys_iOSTests/Macys_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Macys_iOSTests.m
+//  Macys_iOSTests
+//
+//  Created by Alex Cevallos on 6/23/14.
+//  Copyright (c) 2014 AlexCevallos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Macys_iOSTests : XCTestCase
+
+@end
+
+@implementation Macys_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
